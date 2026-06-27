a4183f3de91afa9f0ddd231f1047bbda2b34fee7
Michelle Anderson <manderson0102@yahoo.com>
2026-06-27 12:51:50 -0400

    adding react slick

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Slider from "react-slick";
 
 const HotCollections = () => {
   // 1. Create a state variable to hold the API data
@@ -53,21 +54,29 @@ const HotCollections = () => {
           </div>
           
           {/* 5. Map over the live API data instead of a hardcoded array */}
-          {collections.map((coll) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={coll.id}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  {/* Dynamic link to details page using collection ID */}
-                  <Link to={`/item-details/${coll.nftId}`}>
-                    <img src={coll.nftImage} className="lazy img-fluid" alt={coll.title} />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  {/* Dynamic link to author page using author ID */}
-                  <Link to={`/author/${coll.authorId}`}>
-                    <img className="lazy pp-coll" src={coll.authorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
+          <Slider
+            dots={true}
+            infinite={true}
+            speed={500}
+            slidesToShow={4}
+            slidesToScroll={1}
+          >
+            {collections.map((coll) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={coll.id}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    {/* Dynamic link to details page using collection ID */}
+                    <Link to={`/item-details/${coll.nftId}`}>
+                      <img src={coll.nftImage} className="lazy img-fluid" alt={coll.title} />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    {/* Dynamic link to author page using author ID */}
+                    <Link to={`/author/${coll.authorId}`}>
+                      <img className="lazy pp-coll" src={coll.authorImage} alt="" />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
