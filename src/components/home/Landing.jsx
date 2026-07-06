a91c5e92a7bb87c45c8694bdd2db90b8fb9b898d
Michelle Anderson <manderson0102@yahoo.com>
2026-07-06 17:58:40 -0400

    added fades

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useEffect } from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 const Landing = () => {
-  return (
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
+
+    return (
     <section
       id="section-hero"
       aria-label="section"
@@ -12,35 +21,34 @@ const Landing = () => {
       data-bgimage="url(images/bg-shape-1.jpg) bottom"
       style={{ background: `url(${backgroundImage}) bottom / cover` }}
     >
-      <div className="v-center">
-        <div className="container">
-          <div className="row align-items-center">
+     <div data-aos="fade-up" className="v-center">
+       <div className="container">
+         <div className="row align-items-center">
             <div className="col-md-6">
-              <div className="spacer-single"></div>
+            <div className="spacer-single"></div>
               <h6>
-                <span className="text-uppercase id-color-2">
-                  Ultraverse Market
-                </span>
+                <span className="text-uppercase id-color-2">Ultraverse Market</span>
               </h6>
-              <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
-                Unit of data stored on a digital ledger, called a blockchain,
-                that certifies a digital asset to be unique and therefore not
-                interchangeable
-              </p>
-              <div className="spacer-10"></div>
+           <div className="spacer-10"></div>
+             <h1>Create, sell or collect digital items.</h1>
+               <p className="lead">
+                 Unit of data stored on a digital ledger, called a blockchain, that certifies a digital asset to be unique and therefore not interchangeable
+               </p>
+            <div className="spacer-10"></div>
               <Link className="btn-main lead" to="/explore">
-                Explore
+                   Explore
               </Link>
-              <div className="mb-sm-30"></div>
-            </div>
-            <div className="col-md-6 xs-hide">
-              <img src={NFT} className="lazy img-fluid" alt="" />
-            </div>
-          </div>
+             <div className="mb-sm-30"></div>
+           </div>
+         </div>
         </div>
+        </div>
+        <div className="col-md-6 xs-hide" data-aos="fade-in"> 
+            <img src={NFT} className="lazy img-fluid" alt="" /> 
       </div>
+     </div>
+     </div>
+    </div>
     </section>
   );
 };
